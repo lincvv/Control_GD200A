0479d6d62d169d583a3118795ef531db1b5ec33f
lincvv <lincv@fastmail.com>
2020-11-24 08:20:56 +0200

    add(test wdt)

--- src/main.cpp
+++ src/main.cpp
@@ -187,3 +187,30 @@ void set_state(uint8_t state){
         digitalWrite(PIN_OUT_ON, LOW);
     }
 }
+
+
+//#include <avr/wdt.h>
+//
+//void setup() {
+//    Serial.begin(115200);
+//    Serial.println("Setup..");
+////    Serial.println();
+//    wdt_disable(); // бесполезная строка до которой не доходит выполнение при bootloop
+//
+//    Serial.println("Wait 5 sec..");
+//    delay(5000); // Задержка, чтобы было время перепрошить устройство в случае bootloop
+//    wdt_enable (WDTO_8S); // Для тестов не рекомендуется устанавливать значение менее 8 сек.
+//    Serial.println("Watchdog enabled.");
+//}
+//
+//int timer = 0;
+//
+//void loop(){
+//    // Каждую секунду мигаем светодиодом и значение счетчика пишем в Serial
+//    if(!(millis()%1000)){
+//        timer++;
+//        Serial.println(timer);
+//        digitalWrite(13, digitalRead(13)==1?0:1); delay(1);
+//    }
+////  wdt_reset();
+//}
